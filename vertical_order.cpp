a0058dc3d39a1035b6f4af8a1d81710d4f782b1b
Mohd Faisal <faisal02v@gmail.com>
2023-01-21 12:54:42 +0530

    Update vertical_order.cpp

--- vertical_order.cpp
+++ vertical_order.cpp
@@ -1,3 +1,79 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        queue<pair<TreeNode*, pair<int, int>>>q; //{node, {x,y}}
+        map<int, map<int, multiset<int>>>mp;  //{x, {y,(nodes->vals, set me min val top me hoti he, so)}}
+        vector<vector<int>>ans;
+        if(root==NULL) return ans;
+        
+        q.push({root,{0,0}});
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+
+            
+            int x=it.second.first;
+            int y=it.second.second;
+            TreeNode*node=it.first;
+            if(node->left!=NULL) q.push({node->left, {x-1, y+1}});
+            if(node->right!=NULL) q.push({node->right, {x+1, y+1}});
+            mp[x][y].insert(node->val);
+            
+        }
+        
+        for(auto it:mp){
+            vector<int>v;
+            for(auto jt: it.second){
+                v.insert(v.end(), jt.second.begin(), jt.second.end());
+                // har ek level pr, v ke end me push_back/insert kr rhe he, poora starting se end tak ke multisets
+            }
+            ans.push_back(v);
+        }
+        return ans;
+    }
+};
+
+//https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/submissions/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include <bits/stdc++.h> 
 /************************************************************
 
